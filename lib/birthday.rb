1d6df8babf21affbc8a61b32448e7ff02dc82237
Dakota Wicker <dwicker1996@gmail.com>
2020-03-29 03:06:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,12 +8,12 @@ def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
-end 
+end
 
-def age_appropriate_birthday 
+def age_appropriate_birthday
   if "{age}">= 12
     happy_birthday
-  else 
-      nil 
-  end 
+  else
+      nil
+  end
 end 
